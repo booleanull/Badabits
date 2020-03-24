4f3be674a0736617a0bcd8af0b018ddd300c3603
v.zub <v.zub@clcorp.ru>
2020-03-24 13:45:27 +0300

    bottom navigation view

--- main-feature/src/main/java/com/booleanull/main_feature/ui/NavigationFragment.kt
+++ main-feature/src/main/java/com/booleanull/main_feature/ui/NavigationFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.booleanull.core_ui.ui.BaseFragment
 import com.booleanull.main_feature.R
+import kotlinx.android.synthetic.main.fragment_navigation.*
 
 class NavigationFragment : BaseFragment() {
 
@@ -15,5 +16,25 @@ class NavigationFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_navigation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navigationView.setOnNavigationItemSelectedListener {
+            childFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.navigationContainer, when (it.itemId) {
+                        R.id.action_map -> FirstFragment()
+                        R.id.action_dial -> SecondFragment()
+                        else -> ThirdFragment()
+                    }
+                )
+                .commit()
+            return@setOnNavigationItemSelectedListener true
+        }
+        if (savedInstanceState == null) {
+            navigationView.selectedItemId = R.id.action_map
+        }
     }
 }
