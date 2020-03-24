a71e3dd0086c5e1ff1854e93aaebcdd5a87b0a83
v.zub <v.zub@clcorp.ru>
2020-03-24 12:18:49 +0300

    add core ui elements

---
+++ core-ui/src/main/java/com/booleanull/core_ui/ui/BaseScreen.kt
@@ -0,0 +1,5 @@
+package com.booleanull.core_ui.ui
+
+open class BaseScreen {
+
+}
---
+++ core-ui/src/main/java/com/booleanull/core_ui/ui/BaseViewModel.kt
@@ -0,0 +1,6 @@
+package com.booleanull.core_ui.ui
+
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel: ViewModel() {
+}
